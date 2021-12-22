deb068254deb0b39e4c21c0bc76bedfcb96151db
raeel97 <hecate101@gmail.com>
2021-12-22 15:50:29 +0000

    added home app and templates

--- chocolate_factory/settings.py
+++ chocolate_factory/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account', #Enables basic user account stuff i.e logging in or out, user registration & password resets.
     'allauth.socialaccount', #Enables logging in via social media like Facebook, google etc
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -59,7 +60,10 @@ ROOT_URLCONF = 'chocolate_factory.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
