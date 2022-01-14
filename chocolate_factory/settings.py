c57cce5fc4255be62b17c827b0675f4c7e754842
raeel97 <hecate101@gmail.com>
2022-01-14 16:51:18 +0000

    add checkout views, admin, templates and stripe elements

--- chocolate_factory/settings.py
+++ chocolate_factory/settings.py
@@ -46,6 +46,9 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+    
+    # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -59,6 +62,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'chocolate_factory.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -75,6 +80,13 @@ TEMPLATES = [
                 # request context processor allows allauth & django to access the HTTP request object in templates.
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                                'django.template.context_processors.media',
+                'bag.contexts.bag_contents',
+            ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
                 'bag.contexts.bag_contents',
             ],
         },
