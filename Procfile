48e19d76f54fac282c1847f6983dd01faa9ee2c5
raeel97 <hecate101@gmail.com>
2022-01-17 17:12:55 +0000

    deployed to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn chocolate_factory.wsgi:application
--- chocolate_factory/settings.py
+++ chocolate_factory/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-ku9xawdf%_f$cst8!x=$o%$6m3+yez-g$e8-reobo8_!qg49u+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['chocolate-factory963.herokuapp.com', 'localhost']]
 
 
 # Application definition
@@ -121,12 +122,17 @@ WSGI_APPLICATION = 'chocolate_factory.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
