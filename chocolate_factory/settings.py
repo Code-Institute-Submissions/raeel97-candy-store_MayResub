00f58096a189fe8b86fde7786ea494e48309b46b
raeel97 <hecate101@gmail.com>
2022-01-17 17:27:04 +0000

    removed secret key and set debug

--- chocolate_factory/settings.py
+++ chocolate_factory/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ku9xawdf%_f$cst8!x=$o%$6m3+yez-g$e8-reobo8_!qg49u+'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['chocolate-factory963.herokuapp.com', 'localhost']]
 
