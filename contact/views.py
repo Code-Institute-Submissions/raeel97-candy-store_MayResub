99ff6eaafef718be616d5b85b86d8b035abd7241
raeel97 <hecate101@gmail.com>
2022-01-22 15:29:15 +0000

    finished contact view

--- contact/views.py
+++ contact/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect
+from django.contrib import messages
 from .forms import ContactForm
+from home.views import index
 
 
 def contact_view(request):
     """Display the contact form"""
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            email = request.POST.get('email', '')
+            success = (f"Thank you for your inquiry! Your contact information \
+            and message was successfully submitted. A confirmation email will \
+            be sent to {email}.")
+            messages.success(request, success)
+            return redirect(reverse(index))
     form = ContactForm()
     context = {'form': form}
     return render(request, 'contact/contact.html', context)
