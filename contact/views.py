3d00e47a52ed697c29cbe94f228f10f67be61e81
raeel97 <hecate101@gmail.com>
2022-01-22 16:35:02 +0000

    tweaked email for contact form

--- contact/views.py
+++ contact/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, reverse, redirect
 from django.contrib import messages
 from .forms import ContactForm
 from home.views import index
+from django.core.mail import send_mail
+from chocolate_factory.settings import EMAIL_HOST_USER, EMAIL_HOST_PASSWORD
 
 
 def contact_view(request):
@@ -11,9 +13,21 @@ def contact_view(request):
         if form.is_valid():
             form.save()
             email = request.POST.get('email', '')
+            subject = request.POST.get('subject', '')
+            message_body = request.POST.get('message', '')
+            error = 'Did you fill in the form correctly?'
             success = (f"Thank you for your inquiry! Your contact information \
             and message was successfully submitted. A confirmation email will \
             be sent to {email}.")
+            send_mail(subject, message_body,
+            'chocolatefactory.customercare@gmail.com', [email])
+            # except Exception as e:
+            #     messages.warning(request, error)
+            #     print('not working')
+            #     return redirect(reverse(contact_view))
+            print(f'this is the email: {email}')
+            print(f'this is the subject: {subject}')
+            print(f'this is the message_body: {message_body}')
             messages.success(request, success)
             return redirect(reverse(index))
     form = ContactForm()
