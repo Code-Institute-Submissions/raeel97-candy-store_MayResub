75606ac573d7cb4b33e54b3fbdbffa845b90595d
raeel97 <hecate101@gmail.com>
2022-01-23 16:46:31 +0000

    finished updates functionality

--- updates/urls.py
+++ updates/urls.py
@@ -1,7 +1,8 @@
-from django.contrib import admin
-from django.urls import path, include
 from . import views
+from django.urls import path
 
 urlpatterns = [
-    path('', views.updates_view, name='updates')
+    path('', views.PostList.as_view(), name='updates'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
